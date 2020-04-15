594c8dbfaf0a80ae7a067b37b773650df5925d35
Luke Bennett <hello@lukebennett.com.au>
2020-04-15 17:44:06 +1000

    Style header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { IoMdCart, IoIosArrowDown } from 'react-icons/io';
 import { Link } from 'gatsby';
 
 import { useGraphQL, useCartCount } from '../hooks';
+import { Logo } from './logo';
 
 const Header = () => {
   const {
@@ -12,24 +14,43 @@ const Header = () => {
   const count = useCartCount();
 
   return (
-    <header className="sticky top-0 z-10 bg-white shadow">
-      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+    <header className="sticky z-10 bg-white shadow top-8">
+      <div className="px-4 py-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between">
           <Link to="/">
             <h1 className="text-xl font-semibold leading-none text-gray-900">
-              {title}
+              <span className="sr-only">{title}</span>
+              <Logo className="h-24 text-brand-pink" />
             </h1>
           </Link>
-          <Link
-            to="/cart"
-            className="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md shadow-sm hover:bg-gray-700 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray active:bg-gray-900"
-          >
-            <span className="leading-none">Cart</span>
-
-            <span className="relative inline-flex items-center justify-center w-5 h-5 ml-2 text-sm leading-none text-black bg-white rounded-full">
-              {count}
-            </span>
-          </Link>
+          <div className="flex flex-col items-end">
+            <Link to="/cart" className="flex items-center">
+              <IoMdCart className="text-xl" />
+              <span className="ml-1 text-sm leading-5 tracking-wide uppercase">
+                {count} items
+              </span>
+            </Link>
+            <nav className="mt-2 font-bold uppercase">
+              <Link to="/" className="inline-flex items-center ml-4">
+                Apparel <IoIosArrowDown className="ml-1 text-sm" />
+              </Link>
+              <Link to="/" className="inline-flex items-center ml-4">
+                Brands
+              </Link>
+              <Link to="/" className="inline-flex items-center ml-4">
+                Accessories
+              </Link>
+              <Link to="/" className="inline-flex items-center ml-4">
+                Sale
+              </Link>
+              <Link to="/" className="inline-flex items-center ml-4">
+                FAQ
+              </Link>
+              <Link to="/" className="inline-flex items-center ml-4">
+                Contact
+              </Link>
+            </nav>
+          </div>
         </div>
       </div>
     </header>
