ee4b7fe941158ffe7c9e4ea31dc2b1e61211a097
Luke Bennett <hello@lukebennett.com.au>
2020-04-15 17:44:26 +1000

    Create Topbar

---
+++ src/components/topbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Topbar() {
+  return (
+    <div className="sticky top-0 z-10 flex items-center justify-center h-8 font-bold text-center text-white uppercase bg-brand-pink">
+      Free delivery on all orders over $80
+    </div>
+  );
+}
