9d9e52c68792b3687b41c42f0d57127a9f70a334
Luke Bennett <hello@lukebennett.com.au>
2020-04-15 17:43:35 +1000

    Add basic styles to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,37 +10,25 @@ export default function Footer() {
   return (
     <footer className="bg-white">
       <div className="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="mt-8 md:mt-0 md:order-1">
+        <div className="flex justify-between w-full mt-8 md:mt-0 md:order-1">
           <p className="text-base leading-6 text-center text-gray-400">
             &copy; {new Date().getFullYear()}{' '}
             <a
-              href="https://github.com/lukebennett88/gatsby-starter-shopify-tailwindcss"
+              href="https://golfladiesfirst.com.au"
               className="font-medium text-gray-600 underline transition duration-150 ease-in-out hover:text-gray-500 active:text-gray-800"
             >
               {title}
             </a>
-            . Built with{' '}
+            . All rights reserved.
+          </p>
+          <p>
+            Website by{' '}
             <a
-              href="https://www.gatsbyjs.org"
+              href="https://golfladiesfirst.com.au"
               className="font-medium text-gray-600 underline transition duration-150 ease-in-out hover:text-gray-500 active:text-gray-800"
             >
-              Gatsby
+              Phiranno Designs
             </a>
-            ,{' '}
-            <a
-              href="https://tailwindcss.com"
-              className="font-medium text-gray-600 underline transition duration-150 ease-in-out hover:text-gray-500 active:text-gray-800"
-            >
-              Tailwind
-            </a>{' '}
-            and{' '}
-            <a
-              href="https://www.shopify.ca"
-              className="font-medium text-gray-600 underline transition duration-150 ease-in-out hover:text-gray-500 active:text-gray-800"
-            >
-              Shopify
-            </a>
-            .
           </p>
         </div>
       </div>
