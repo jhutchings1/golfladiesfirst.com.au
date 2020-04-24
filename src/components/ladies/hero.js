c2973377c1ecdab70027ef4d57cb5acea09735e2
Luke Bennett <hello@lukebennett.com.au>
2020-04-24 15:18:45 +1000

    Make hero responsive

--- src/components/ladies/hero.js
+++ src/components/ladies/hero.js
@@ -10,34 +10,45 @@ const Hero = () => {
   return (
     <article className="overflow-hidden bg-white">
       <div className="relative w-full mx-auto max-w-7xl">
-        <div className="absolute inset-0 h-full">
-          <GatsbyImage fluid={heroImage.childImageSharp.fluid} />
+        <div className="absolute inset-0 hidden h-full md:block">
+          <GatsbyImage
+            fluid={heroImage.childImageSharp.fluid}
+            className="h-full"
+          />
         </div>
-        <div className="relative flex bg-transparent-white-25">
-          <div className="py-12 text-center md:w-1/2">
-            <h1 className="font-bold leading-none text-center text-black uppercase">
-              <span className="text-6xl">20% off </span>
-              <br />
-              <span>head to toe looks</span>
-            </h1>
-            <p className="uppercase">Discount applied at checkout</p>
+        <div className="relative flex flex-wrap bg-transparent-white-25">
+          <div className="flex w-full text-center md:w-1/2">
+            <div className="absolute inset-0 h-full md:hidden">
+              <GatsbyImage
+                fluid={heroImage.childImageSharp.fluid}
+                className="h-full"
+              />
+            </div>
+            <div className="relative flex-1 py-12 bg-transparent-white-25 md:bg-transparent">
+              <h1 className="font-bold leading-none text-center text-black uppercase">
+                <span className="text-6xl">20% off </span>
+                <br />
+                <span>head to toe looks</span>
+              </h1>
+              <p className="uppercase">Discount applied at checkout</p>
+            </div>
           </div>
-          <span className="mt-auto ml-auto overflow-hidden md:inline-flex">
+          <div className="w-full mt-auto ml-auto overflow-hidden md:inline-flex md:w-1/2">
             <Link to="/ladies/" className="w-full md:w-1/2 bg-brand-pink">
-              <span className="flex items-center justify-center transform -skew-x-12 bg-brand-pink">
-                <h3 className="px-12 py-4 text-sm font-bold tracking-wider text-white uppercase whitespace-no-wrap transform skew-x-12 px4">
+              <span className="flex items-center justify-center transform md:-skew-x-12 bg-brand-pink">
+                <h3 className="px-12 py-4 text-sm font-bold tracking-wider text-white uppercase whitespace-no-wrap transform md:skew-x-12 px4">
                   Shop Ladies
                 </h3>
               </span>
             </Link>
             <Link to="/men/" className="w-full md:w-1/2 bg-brand-blue">
-              <span className="flex items-center justify-center transform -skew-x-12 bg-brand-blue">
-                <h3 className="px-12 py-4 text-sm font-bold tracking-wider text-white uppercase whitespace-no-wrap transform skew-x-12 px4">
+              <span className="flex items-center justify-center transform md:-skew-x-12 bg-brand-blue">
+                <h3 className="px-12 py-4 text-sm font-bold tracking-wider text-white uppercase whitespace-no-wrap transform md:skew-x-12 px4">
                   Shop Mens
                 </h3>
               </span>
             </Link>
-          </span>
+          </div>
         </div>
       </div>
     </article>
