f5ae3e7ddfd9e868703d60b87cc045a9edc7d7c6
Luke Bennett <hello@lukebennett.com.au>
2020-04-25 23:34:50 +1000

    Add mens page

---
+++ src/pages/mens.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Layout, SEO } from '../components';
+import { Hero, ProductWrapper } from '../components/ladies';
+
+export default function MensPage() {
+  return (
+    <Layout theme="mens">
+      <SEO title="Men's" />
+      <Hero />
+      <ProductWrapper />
+    </Layout>
+  );
+}
