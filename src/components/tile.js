96a76eaf9d6afe9027503cf11bec4f6e54e12b4d
Luke Bennett <hello@lukebennett.com.au>
2020-04-29 08:59:39 +1000

    use LazyLoad hook

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,32 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
-import { useInView } from 'react-intersection-observer';
 
-import { useGraphQL } from '../hooks';
+import { useGraphQL, useLazyLoad } from '../hooks';
 import { resizeShopifyImage } from '../utilities';
-import { Spinner } from './spinner';
 
 export function Tile({ title, slug, price, image }) {
   const { placeholderImage } = useGraphQL();
-  const [imgLoaded, setImgLoaded] = useState(false);
 
-  const [ref, inView] = useInView({
-    threshold: 0,
-    triggerOnce: true,
-  });
-
-  const imgRef = useRef(null);
   const imageSrc = image
     ? image.originalSrc &&
       resizeShopifyImage({ url: image.originalSrc, width: 300 })
     : placeholderImage.publicURL;
 
-  useEffect(() => {
-    if (inView) {
-      imgRef.current.src = imgRef.current.dataset.src;
-    }
-  }, [inView]);
+  const { ref, imgRef, isImgLoaded, handleImgLoaded, Spinner } = useLazyLoad();
 
   return (
     <Link
@@ -37,16 +24,12 @@ export function Tile({ title, slug, price, image }) {
       <div className="relative h-64">
         <img
           ref={imgRef}
+          onLoad={handleImgLoaded}
           data-src={imageSrc}
-          onLoad={() => setImgLoaded(true)}
           alt=""
           className="object-contain w-full h-full duration-500 ease-in-out transform group-hover:scale-110 group-focus:scale-110"
         />
-        {!imgLoaded && (
-          <div className="absolute inset-0 flex items-center justify-center w-full h-full bg-gray-50">
-            <Spinner />
-          </div>
-        )}
+        {!isImgLoaded && <Spinner />}
       </div>
       <div className="p-6">
         <h3 className="mt-2 line-clamp">{title}</h3>
@@ -64,9 +47,4 @@ Tile.propTypes = {
   price: PropTypes.number,
   slug: PropTypes.string,
   title: PropTypes.string,
-};
-
-Tile.defaultProps = {
-  title: "Men's Down Jacket",
-  price: '50',
 };
