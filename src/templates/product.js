ccca726cc4a9a1a128ab77b001621fd12e1420bc
Luke Bennett <hello@lukebennett.com.au>
2020-04-28 15:32:43 +1000

    Add option pickers and add lots of comments

--- src/templates/product.js
+++ src/templates/product.js
@@ -4,34 +4,91 @@ import PropTypes from 'prop-types';
 import { useInView } from 'react-intersection-observer';
 
 import { useAddItemToCart, useGraphQL } from '../hooks';
-import { prepareVariantsWithOptions } from '../utilities';
-import { Layout, SEO, Alert, Spinner } from '../components';
+import {
+  prepareVariantsWithOptions,
+  prepareVariantsImages,
+} from '../utilities';
+import {
+  Layout,
+  SEO,
+  Alert,
+  Spinner,
+  OptionPicker,
+  Thumbnail,
+} from '../components';
 
 export default function ProductPage({ data: { shopifyProduct: product } }) {
-  const [imgLoaded, setImgLoaded] = useState(false);
+  // Get available colours
+  const colours =
+    product.options.find((option) => option.name.toLowerCase() === 'colour')
+      ?.values || [];
 
-  const { placeholderImage } = useGraphQL();
+  // Get available sizes
+  const sizes =
+    product.options.find((option) => option.name.toLowerCase() === 'size')
+      ?.values || [];
 
-  const [ref, inView] = useInView({
-    threshold: 0,
-    triggerOnce: true,
-  });
-
-  const imgRef = useRef(null);
-
+  // Format the data we get back from GraphQL for variants to be a little easier to work with
+  // See comment in `prepare-variants-with-options.js`
   const variants = useMemo(() => prepareVariantsWithOptions(product.variants), [
     product.variants,
   ]);
 
-  const addItemToCart = useAddItemToCart();
-  const variant = variants[0];
+  // Format the data we get back from GraphQL for images to be a little easier to work with
+  // See comment in `prepare-variants-images.js`
+  const images = useMemo(() => prepareVariantsImages(variants, 'colour'), [
+    variants,
+  ]);
+
+  // Keep variants in state, and set the default variant to be the first item
+  const [variant, setVariant] = useState(variants[0]);
+
+  // Keep different colour options in state
+  const [colour, setColour] = useState(variant.colour);
+
+  // Keep different sizes in state
+  const [size, setSize] = useState(variant.size);
+
+  // Manage add to cart alerts in state
   const [addedToCartMessage, setAddedToCartMessage] = useState(null);
 
+  // Use a custom hook for adding items to cart
+  const addItemToCart = useAddItemToCart();
+
+  // Whenever we add an item to the cart, also create an alert to notify the customer of this
+  // Note: we are hard coding the number of items to be added to cart as 1, we can add another useState instance to address this in the future if we need to
   function handleAddToCart() {
     addItemToCart(variant.shopifyId, 1);
     setAddedToCartMessage('Added to your cart!');
   }
 
+  // This handles adding the correct variant to the cart
+  useEffect(() => {
+    const newVariant = variants.find((v) => {
+      return v.size === size && v.colour === colour;
+    });
+
+    if (variant.shopifyId !== newVariant.shopifyId) {
+      setVariant(newVariant);
+    }
+  }, [size, colour, variants, variant.shopifyId]);
+
+  // Add a ref to image wrapper so that we can detect when it enters the viewport using IntersectionObserver
+  const [ref, inView] = useInView({
+    threshold: 0,
+    triggerOnce: true,
+  });
+
+  // Create a ref for the product image
+  const imgRef = useRef(null);
+
+  // If product doesn't have an image, we can use a placeholder
+  const { placeholderImage } = useGraphQL();
+
+  // Show a spinner until `imgLoaded === true`
+  const [imgLoaded, setImgLoaded] = useState(false);
+
+  // When image enters the screen swap out src for the data-src
   useEffect(() => {
     if (inView) {
       imgRef.current.src = imgRef.current.dataset.src;
@@ -49,31 +106,64 @@ export default function ProductPage({ data: { shopifyProduct: product } }) {
               dismiss={() => setAddedToCartMessage(null)}
             />
           )}
-          <div
-            ref={ref}
-            className="relative h-0 mt-2 overflow-hidden aspect-ratio-square"
-          >
-            <img
-              ref={imgRef}
-              data-src={
-                variant.image
-                  ? variant.image.originalSrc
-                  : placeholderImage.publicURL
-              }
-              onLoad={() => setImgLoaded(true)}
-              alt=""
-              width={592}
-              height={592}
-              className="absolute inset-0 object-cover h-full mx-auto overflow-hidden duration-500 ease-in-out transform hover:scale-110"
-            />
-            {!imgLoaded && (
-              <div className="absolute inset-0 flex items-center justify-center w-full h-full bg-gray-50">
-                <Spinner />
+          <div>
+            <div
+              ref={ref}
+              className="relative h-0 mt-2 overflow-hidden aspect-ratio-square"
+            >
+              <img
+                ref={imgRef}
+                data-src={
+                  variant.image
+                    ? variant.image.originalSrc
+                    : placeholderImage.publicURL
+                }
+                onLoad={() => setImgLoaded(true)}
+                alt=""
+                width={592}
+                height={592}
+                className="absolute inset-0 object-contain h-full mx-auto overflow-hidden duration-500 ease-in-out transform hover:scale-110"
+              />
+              {!imgLoaded && (
+                <div className="absolute inset-0 flex items-center justify-center w-full h-full bg-gray-50">
+                  <Spinner />
+                </div>
+              )}
+            </div>
+            {images.length > 1 && (
+              <div className="grid">
+                {images.map((img) => (
+                  <Thumbnail
+                    key={img.colour}
+                    src={img.src}
+                    onClick={() => setColour(colour)}
+                  />
+                ))}
               </div>
             )}
           </div>
           <div className="mt-12">
             <h1 className="h2">{product.title}</h1>
+            <div className="grid gap-4 mt-6 sm:grid-cols-2">
+              {colours.length > 1 && (
+                <OptionPicker
+                  key="Colour"
+                  name="Colour"
+                  options={colours}
+                  selected={colour}
+                  onChange={(event) => setColour(event.target.value)}
+                />
+              )}
+              {sizes.length > 1 && (
+                <OptionPicker
+                  key="Size"
+                  name="Size"
+                  options={sizes}
+                  selected={size}
+                  onChange={(event) => setSize(event.target.value)}
+                />
+              )}
+            </div>
             <span className="inline-flex mt-6 shadow-sm">
               <button
                 onClick={handleAddToCart}
