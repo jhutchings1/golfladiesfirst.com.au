e23099907149ab7c25c625c4fbecae363e5b8244
Luke Bennett <hello@lukebennett.com.au>
2020-04-28 15:20:21 +1000

    Style option picker

--- src/components/option-picker.js
+++ src/components/option-picker.js
@@ -5,12 +5,12 @@ export const OptionPicker = ({ name, options, onChange, selected }) => {
   return (
     <div>
       <label htmlFor={name.toLowerCase()}>
-        <span>{name}</span>
+        <div>{name}</div>
         <select
           onChange={onChange}
           value={selected}
           id={name.toLowerCase()}
-          className="form-select"
+          className="w-full max-w-xs transition duration-150 ease-in-out rounded-none form-select sm:text-sm sm:leading-5 focus:outline-none focus:shadow-outline-primary focus:border-primary-light"
         >
           {options.map((option) => (
             <option value={option} key={option}>
