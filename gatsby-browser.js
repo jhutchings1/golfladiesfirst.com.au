b88203b1efff4b48338c38204d86f82d5827fefc
Luke Bennett <hello@lukebennett.com.au>
2020-05-20 12:48:45 +1000

    Add smooth scroll

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,7 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
+import smoothScroll from 'smooth-scroll';
 
 import { StoreContextProvider } from './src/context/store-context';
 import 'typeface-inter';
@@ -14,6 +15,12 @@ import './src/css/tailwind.css';
 export const wrapRootElement = ({ element }) => (
   <StoreContextProvider>{element}</StoreContextProvider>
 );
+
+// https://medium.com/@chrisfitkin/how-to-smooth-scroll-links-in-gatsby-3dc445299558
+if (typeof window !== 'undefined') {
+  // eslint-disable-next-line global-require
+  smoothScroll('a[href*="#"]');
+}
 
 wrapRootElement.propTypes = {
   element: PropTypes.node.isRequired,
