be5c4a73a4331f4d1de7a230aac5889f435b81f3
NOP-slide <phelps.andrewj@gmail.com>
2020-06-02 12:34:53 +0930

    fix

--- src/templates/product.js
+++ src/templates/product.js
@@ -145,8 +145,6 @@ export default function ProductPage({ data: { shopifyProduct: product } }) {
           // );
           setAddToCartDisabled(true);
         }
-      } else {
-        setAddToCartDisabled(false);
       }
     } else {
       // If variant doesn't exist
