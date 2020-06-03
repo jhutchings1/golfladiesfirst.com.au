73e5216afec2e3a42310d2126756bbc2b8398572
Luke Bennett <hello@lukebennett.com.au>
2020-06-03 15:20:51 +1000

    Wrap text to a more readable width

--- src/pages/refund-policy.js
+++ src/pages/refund-policy.js
@@ -9,13 +9,15 @@ export default function RefundPolicy() {
     <Layout>
       <SEO title="Refund Policy" />
       <article className="w-full px-4 py-16 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <h1 className="h2">{allShopifyShopPolicy.nodes[0].title}</h1>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: allShopifyShopPolicy.nodes[0].body,
-          }}
-          className="grid gap-4 mt-6"
-        />
+        <div className="w-full max-w-2xl mx-auto">
+          <h1 className="h2">{allShopifyShopPolicy.nodes[0].title}</h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: allShopifyShopPolicy.nodes[0].body,
+            }}
+            className="mt-6 prose"
+          />
+        </div>
       </article>
     </Layout>
   );
