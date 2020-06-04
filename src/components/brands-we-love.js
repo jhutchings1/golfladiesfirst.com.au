7167c2d6d530a9a03b7e3096a8ef4c5a8f89558d
Luke Bennett <hello@lukebennett.com.au>
2020-06-04 12:53:35 +1000

    Refactor brands section

--- src/components/brands-we-love.js
+++ src/components/brands-we-love.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import annika from '../images/brand-logos/annika.svg';
-import bermudaSands from '../images/brand-logos/bermuda-sands.svg';
-import callaway from '../images/brand-logos/callaway.svg';
-import corsicanGolf from '../images/brand-logos/corsican-golf.svg';
-import dailySports from '../images/brand-logos/daily-sports.svg';
-import eccoShoes from '../images/brand-logos/ecco-shoes.svg';
-import fit39Gloves from '../images/brand-logos/fit39-gloves.svg';
-import gregNorman from '../images/brand-logos/greg-norman.svg';
-import jamieSadock from '../images/brand-logos/jamie-sadock.svg';
-import nivo from '../images/brand-logos/nivo.svg';
-import skechers from '../images/brand-logos/skechers.svg';
-import sporteLeisure from '../images/brand-logos/sporte-leisure.svg';
+import { brands } from '../data/brands';
 
 export function BrandsWeLove() {
   return (
@@ -20,90 +9,20 @@ export function BrandsWeLove() {
       <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 sm:py-16 lg:px-8">
         <h2 className="h2">Shop brands we love</h2>
         <div className="grid grid-cols-2 mt-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 lg:mt-8">
-          <Link
-            to="/collections/annika/"
-            className="flex justify-center col-span-1 px-8 py-8 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:shadow-outline-primary focus:bg-gray-100"
-          >
-            <span className="sr-only">Annika</span>
-            <img className="max-h-12" src={annika} alt="" />
-          </Link>
-          <Link
-            to="/collections/bermuda-sands-brand/"
-            className="flex justify-center col-span-1 px-8 py-8 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:shadow-outline-primary focus:bg-gray-100"
-          >
-            <span className="sr-only">Bermuda Sands</span>
-            <img className="max-h-12" src={bermudaSands} alt="" />
-          </Link>
-          <Link
-            to="/collections/callaway/"
-            className="flex justify-center col-span-1 px-8 py-8 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:shadow-outline-primary focus:bg-gray-100"
-          >
-            <span className="sr-only">Callaway</span>
-            <img className="max-h-12" src={callaway} alt="" />
-          </Link>
-          <Link
-            to="/collections/corsican-golf/"
-            className="flex justify-center col-span-1 px-8 py-8 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:shadow-outline-primary focus:bg-gray-100"
-          >
-            <span className="sr-only">Corsican Golf</span>
-            <img className="max-h-12" src={corsicanGolf} alt="" />
-          </Link>
-          <Link
-            to="/collections/daily-sports/"
-            className="flex justify-center col-span-1 px-8 py-8 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:shadow-outline-primary focus:bg-gray-100"
-          >
-            <span className="sr-only">Daily Sports</span>
-            <img className="max-h-12" src={dailySports} alt="" />
-          </Link>
-          <Link
-            to="/collections/ecco-shoes/"
-            className="flex justify-center col-span-1 px-8 py-8 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:shadow-outline-primary focus:bg-gray-100"
-          >
-            <span className="sr-only">Ecco Shoes</span>
-            <img className="max-h-12" src={eccoShoes} alt="" />
-          </Link>
-          <Link
-            to="/collections/fit39-gloves-brand/"
-            className="flex justify-center col-span-1 px-8 py-8 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:shadow-outline-primary focus:bg-gray-100"
-          >
-            <span className="sr-only">Fit 39 Gloves</span>
-            <img className="max-h-12" src={fit39Gloves} alt="" />
-          </Link>
-          <Link
-            to="/collections/greg-norman/"
-            className="flex justify-center col-span-1 px-8 py-8 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:shadow-outline-primary focus:bg-gray-100"
-          >
-            <span className="sr-only">Greg Norman</span>
-            <img className="max-h-12" src={gregNorman} alt="" />
-          </Link>
-          <Link
-            to="/collections/jamie-sadock-brand/"
-            className="flex justify-center col-span-1 px-8 py-8 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:shadow-outline-primary focus:bg-gray-100"
-          >
-            <span className="sr-only">Jamie Sadock</span>
-            <img className="max-h-12" src={jamieSadock} alt="" />
-          </Link>
-          <Link
-            to="/collections/nivo/"
-            className="flex justify-center col-span-1 px-8 py-8 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:shadow-outline-primary focus:bg-gray-100"
-          >
-            <span className="sr-only">Nivo</span>
-            <img className="max-h-12" src={nivo} alt="" />
-          </Link>
-          <Link
-            to="/collections/skechers-shoes/"
-            className="flex justify-center col-span-1 px-8 py-8 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:shadow-outline-primary focus:bg-gray-100"
-          >
-            <span className="sr-only">Skechers</span>
-            <img className="max-h-12" src={skechers} alt="" />
-          </Link>
-          <Link
-            to="/collections/sporte-leisure/"
-            className="flex justify-center col-span-1 px-8 py-8 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:shadow-outline-primary focus:bg-gray-100"
-          >
-            <span className="sr-only">Sporte Leisure</span>
-            <img className="max-h-12" src={sporteLeisure} alt="" />
-          </Link>
+          {brands.map((brand) => (
+            <Link
+              key={brand.id}
+              to={brand.slug}
+              className="relative flex justify-center col-span-1 px-8 py-8 transition duration-150 ease-in-out rounded hover:bg-gray-100 focus:outline-none focus:shadow-outline-primary focus:bg-gray-100 focus:z-10"
+            >
+              <span className="sr-only">{brand.label}</span>
+              <img
+                className="max-h-12"
+                src={brand.icon}
+                alt={`${brand.label} logo`}
+              />
+            </Link>
+          ))}
         </div>
       </div>
     </article>
---
+++ src/data/brands.js
@@ -0,0 +1,87 @@
+import annika from '../images/brand-logos/annika.svg';
+import bermudaSands from '../images/brand-logos/bermuda-sands.svg';
+import callaway from '../images/brand-logos/callaway.svg';
+import corsicanGolf from '../images/brand-logos/corsican-golf.svg';
+import dailySports from '../images/brand-logos/daily-sports.svg';
+import eccoShoes from '../images/brand-logos/ecco-shoes.svg';
+import fit39Gloves from '../images/brand-logos/fit39-gloves.svg';
+import gregNorman from '../images/brand-logos/greg-norman.svg';
+import jamieSadock from '../images/brand-logos/jamie-sadock.svg';
+import nivo from '../images/brand-logos/nivo.svg';
+import skechers from '../images/brand-logos/skechers.svg';
+import sporteLeisure from '../images/brand-logos/sporte-leisure.svg';
+
+export const brands = [
+  {
+    id: '84124b06-7e47-46af-9707-7c94fcbe4963',
+    label: 'Annika',
+    slug: '/collections/annika/',
+    icon: annika,
+  },
+  {
+    id: 'f13b50e3-40af-4829-affa-60de70959942',
+    label: 'Bermuda Sands',
+    slug: '/collections/bermuda-sands-brand/',
+    icon: bermudaSands,
+  },
+  {
+    id: '1a6e692f-3bf1-4ec5-8aaa-65de4d8e0135',
+    label: 'Callaway',
+    slug: '/collections/callaway/',
+    icon: callaway,
+  },
+  {
+    id: '13145de5-7723-42b3-a4eb-20a069e7f29f',
+    label: 'Corsican Golf',
+    slug: '/collections/corsican-golf/',
+    icon: corsicanGolf,
+  },
+  {
+    id: '9ff806fd-9dda-4f54-9b9e-94d7a2b57b0c',
+    label: 'Daily Sports',
+    slug: '/collections/daily-sports/',
+    icon: dailySports,
+  },
+  {
+    id: 'e5af315e-8aa9-4f72-b3ab-4b9761e57550',
+    label: 'Ecco Shoes',
+    slug: '/collections/ecco-shoes/',
+    icon: eccoShoes,
+  },
+  {
+    id: '901dd4ca-8d75-4a62-95ab-c3fcb6c0e44e',
+    label: 'Fit 39 Gloves',
+    slug: '/collections/fit39-gloves-brand/',
+    icon: fit39Gloves,
+  },
+  {
+    id: 'c8562edd-ac70-43ca-96b5-893184c4316b',
+    label: 'Greg Norman',
+    slug: '/collections/greg-norman/',
+    icon: gregNorman,
+  },
+  {
+    id: 'a9a83b6b-4892-433c-afeb-49d4ea473079',
+    label: 'Jamie Sadock',
+    slug: '/collections/jamie-sadock-brand/',
+    icon: jamieSadock,
+  },
+  {
+    id: 'b0b53462-aefd-40fc-9abb-50696fc80dde',
+    label: 'Nivo',
+    slug: '/collections/nivo/',
+    icon: nivo,
+  },
+  {
+    id: 'd6a18cab-2326-4223-9661-a51ff5dfe2a8',
+    label: 'Skechers',
+    slug: '/collections/skechers-shoes/',
+    icon: skechers,
+  },
+  {
+    id: 'b1e897cb-740d-4958-9dbd-bc3ab8d5fd7f',
+    label: 'Sporte Leisure',
+    slug: '/collections/sporte-leisure/',
+    icon: sporteLeisure,
+  },
+];
