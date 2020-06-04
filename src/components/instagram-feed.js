f9e3111103e01becbaf7968b58108225b56926d0
Luke Bennett <hello@lukebennett.com.au>
2020-06-04 22:40:58 +1000

    Constrict size of Instagram feed

--- src/components/instagram-feed.js
+++ src/components/instagram-feed.js
@@ -4,9 +4,9 @@ import { InstagramImages } from './instagram';
 
 export function InstagramFeed() {
   return (
-    <article className="py-6 bg-white">
+    <article className="w-full px-4 pt-16 pb-6 mx-auto bg-white max-w-7xl sm:px-6 lg:px-8">
       <h2 className="px-4 text-center h2">Keep social with us</h2>
-      <div className="grid grid-cols-2 gap-4 mt-6 md:grid-cols-3 lg:grid-cols-6">
+      <div className="grid grid-cols-2 gap-4 mt-8 md:grid-cols-3 lg:grid-cols-6">
         <InstagramImages />
       </div>
     </article>
