b5afb5937820cad2eb2d3285a95d4130dfa52134
Luke Bennett <hello@lukebennett.com.au>
2020-06-05 16:38:08 +1000

    Delete unwanted sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,19 @@ import {
   Layout,
   SEO,
   BrandsWeLove,
-  OurStory,
   Contact,
   NewsletterSignup,
   InstagramFeed,
   Map,
 } from '../components';
-import { CollectionLatest, CollectionPromo } from '../components/collections';
+import { CollectionPromo } from '../components/collections';
 
 export default function IndexPage() {
   return (
     <Layout hasHero>
       <SEO title="Home" />
-      <CollectionLatest />
       <CollectionPromo />
       <BrandsWeLove />
-      <OurStory />
       <Contact />
       <NewsletterSignup />
       <InstagramFeed />
