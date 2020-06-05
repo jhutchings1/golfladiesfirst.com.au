18fb4d9a473bcc88ba3d8826bfceab22598684d6
Luke Bennett <hello@lukebennett.com.au>
2020-06-05 16:37:46 +1000

    Delete brands

--- src/data/brands.js
+++ src/data/brands.js
@@ -1,15 +1,12 @@
 import annika from '../images/brand-logos/annika.svg';
 import bermudaSands from '../images/brand-logos/bermuda-sands.svg';
-import callaway from '../images/brand-logos/callaway.svg';
 import corsicanGolf from '../images/brand-logos/corsican-golf.svg';
 import dailySports from '../images/brand-logos/daily-sports.svg';
 import eccoShoes from '../images/brand-logos/ecco-shoes.svg';
 import fit39Gloves from '../images/brand-logos/fit39-gloves.svg';
-import gregNorman from '../images/brand-logos/greg-norman.svg';
 import jamieSadock from '../images/brand-logos/jamie-sadock.svg';
 import nivo from '../images/brand-logos/nivo.svg';
 import skechers from '../images/brand-logos/skechers.svg';
-import sporteLeisure from '../images/brand-logos/sporte-leisure.svg';
 
 export const brands = [
   {
@@ -23,12 +20,6 @@ export const brands = [
     label: 'Bermuda Sands',
     slug: '/collections/bermuda-sands-brand/',
     icon: bermudaSands,
-  },
-  {
-    id: '1a6e692f-3bf1-4ec5-8aaa-65de4d8e0135',
-    label: 'Callaway',
-    slug: '/collections/callaway/',
-    icon: callaway,
   },
   {
     id: '13145de5-7723-42b3-a4eb-20a069e7f29f',
@@ -55,12 +46,6 @@ export const brands = [
     icon: fit39Gloves,
   },
   {
-    id: 'c8562edd-ac70-43ca-96b5-893184c4316b',
-    label: 'Greg Norman',
-    slug: '/collections/greg-norman/',
-    icon: gregNorman,
-  },
-  {
     id: 'a9a83b6b-4892-433c-afeb-49d4ea473079',
     label: 'Jamie Sadock',
     slug: '/collections/jamie-sadock-brand/',
@@ -77,11 +62,5 @@ export const brands = [
     label: 'Skechers',
     slug: '/collections/skechers-shoes/',
     icon: skechers,
-  },
-  {
-    id: 'b1e897cb-740d-4958-9dbd-bc3ab8d5fd7f',
-    label: 'Sporte Leisure',
-    slug: '/collections/sporte-leisure/',
-    icon: sporteLeisure,
   },
 ];
