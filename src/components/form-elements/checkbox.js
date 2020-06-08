c339c57de61755374f8efb62ec58024103e0cc31
Luke Bennett <hello@lukebennett.com.au>
2020-06-08 17:48:37 +1000

    Make checkbox stop form from submitting if it isn't checked

--- src/components/form-elements/checkbox.js
+++ src/components/form-elements/checkbox.js
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import React, { useRef } from 'react';
 
 import { useEventListener } from '../../hooks';
 
-export function CheckBox() {
-  const [isChecked, setChecked] = useState(false);
+export function CheckBox({ isChecked, setIsChecked, notCheckedMessage }) {
+  const checkboxRef = useRef();
 
   const isBrowser = typeof window !== 'undefined';
 
   function handleKeydown(e) {
-    if (e.keyCode === 32) {
+    if (checkboxRef.current === e.target && e.keyCode === 32) {
       e.preventDefault();
-      setChecked(!isChecked);
+      setIsChecked(!isChecked);
     }
   }
 
@@ -21,22 +22,33 @@ export function CheckBox() {
   return (
     <div className="flex-shrink-0">
       <span
+        ref={checkboxRef}
         role="checkbox"
         tabIndex="0"
-        onClick={() => setChecked(!isChecked)}
+        onClick={() => setIsChecked(!isChecked)}
         onKeyDown={(e) => handleKeydown(e)}
         aria-checked={isChecked}
         className={`${
-          isChecked ? 'bg-gray-800' : 'bg-gray-200'
-        } relative flex-shrink-0 inline-block h-6 transition duration-200 ease-in-out border-2 border-transparent rounded-full cursor-pointer w-11 focus:outline-none focus:shadow-outline-gray`}
+          isChecked ? 'bg-gray-800' : ''
+        } relative flex-shrink-0 inline-block h-6 transition duration-200 ease-in-out border-2 border-transparent rounded-full cursor-pointer w-11 focus:outline-none ${
+          notCheckedMessage
+            ? 'bg-red-200 focus:shadow-outline-red'
+            : 'bg-gray-200 focus:shadow-outline-gray'
+        }`}
       >
         <span
           aria-hidden="true"
           className={`${
-            isChecked ? 'translate-x-0' : 'translate-x-5'
+            isChecked ? 'translate-x-5' : 'translate-x-0'
           } inline-block w-5 h-5 transition duration-200 ease-in-out transform bg-white rounded-full shadow`}
         />
       </span>
     </div>
   );
 }
+
+CheckBox.propTypes = {
+  isChecked: PropTypes.bool.isRequired,
+  setIsChecked: PropTypes.func.isRequired,
+  notCheckedMessage: PropTypes.bool.isRequired,
+};
