8b92850e31406b395112223c6d42be044da0be5e
Luke Bennett <hello@lukebennett.com.au>
2020-06-10 09:48:43 +1000

    Fix mobile layout

--- src/components/ladies/ladies-greg-norman.js
+++ src/components/ladies/ladies-greg-norman.js
@@ -11,7 +11,7 @@ export function LadiesGregNorman() {
       to="/collections/greg-norman/?filter=ladies"
       width="large"
     >
-      <div className="flex flex-col items-center w-2/5 mr-auto">
+      <div className="flex flex-col items-center lg:mr-auto lg:w-2/5">
         <span className="inline-flex mt-6 shadow-sm">
           <div className="inline-flex items-center px-6 py-3 text-base font-bold leading-6 text-black uppercase transition duration-150 ease-in-out bg-white border border-transparent hover:text-gray-700 focus:outline-none focus:border-primary focus:shadow-outline-primary active:text-gray-800 active:bg-gray-50">
             Shop Greg Norman
